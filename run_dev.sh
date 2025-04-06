1a8870b43ff3c17a35eabebe231b2f92c8eee72e
VThnhTng <tung5112002@gmail.com>
2025-04-06 09:48:32 +0700

    Add docker volume config

--- run_dev.sh
+++ run_dev.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
+echo "run npm install"
 npm install
 # Create MySQL Docker Container
+
+echo "run mysql container"
 ./run_mysql_container.sh
 
+echo "run migrations"
 # Migrate Database
 npx prisma migrate dev
 
+
+sleep 5
+echo "run seed"
 # Seed data
 npx prisma db seed
 
